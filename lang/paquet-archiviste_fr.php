8ff80032d285ec8394104d2f0a39fcbdae3ab6ea
salvatore <salvatore@rezo.net>
2017-03-21 04:32:44 +0000

    [Salvatore] [source:_core_/plugins/archiviste/lang/ paquet-archiviste] Export depuis http://trad.spip.net
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/archiviste@103420 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lang/paquet-archiviste_fr.php
+++ lang/paquet-archiviste_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/archiviste/lang/
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
@@ -10,5 +10,5 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'archiviste_description' => 'Permet d’emballer ou déballer des archives de fichiers Zip, Tar, …',
 	'archiviste_nom' => 'Archiviste',
-	'archiviste_slogan' => 'Gérer des archives de fichiers',
+	'archiviste_slogan' => 'Gérer des archives de fichiers'
 );
